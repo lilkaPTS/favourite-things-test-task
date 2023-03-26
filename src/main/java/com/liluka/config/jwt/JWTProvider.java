ed5c0116d364c155273f85f29b0f973d7c722236
lilkaPTS <61536273+lilkaPTS@users.noreply.github.com>
2023-03-26 17:06:51 +0300

    minor improvements

--- src/main/java/com/liluka/config/jwt/JWTProvider.java
+++ src/main/java/com/liluka/config/jwt/JWTProvider.java
@@ -4,7 +4,6 @@ import com.liluka.exception.JWTAuthenticationException;
 import com.liluka.service.security.UserDetailsServiceImpl;
 import io.jsonwebtoken.*;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
--- src/main/java/com/liluka/service/api/AuthorizationService.java
+++ src/main/java/com/liluka/service/api/AuthorizationService.java
@@ -1,9 +1,6 @@
 package com.liluka.service.api;
 
 import com.liluka.dto.LoginDTO;
-import org.springframework.http.ResponseEntity;
-
-import java.util.Map;
 
 public interface AuthorizationService {
     String login(LoginDTO dto);
